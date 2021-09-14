ccfd98b7468f6968d2135d5df86928884df215c3
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-14 13:54:14 -0700

    commit tree 35c811c63fe276b2a6246066ec1403918cb53fab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Muliply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Random
+const random = Math.ceil(Math.random() * 20);
+
+// Remainder
+const num3 = 24;
+const num4 = 20;
+const mod = num3 % num4;
+
+// Max
+const max = Math.max(2, 4, 7, 8, 20);
